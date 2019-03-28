4f1a4e39051757ccabc9f301e52189aae5a6edd7
dietrich03muller <46943628+dietrich03muller@users.noreply.github.com>
2019-03-28 23:01:31 +0000

    Add files via upload

---
+++ blink.bat
@@ -0,0 +1,20 @@
+@echo off
+:a
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+color 19
+color f5
+color c7
+color 06
+color 1a
+color 2b
+color bf
+color 83
+color e6
+color 08
+color f5
+color cd
+color a7
+color 5f
+color ac
+color b9
+goto a
